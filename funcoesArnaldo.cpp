0c418d923c7e5cbbe4c8471ef8cf6604dddea1fb
Arnaldo490 <arnaldoluizmagalhaes123@gmail.com>
2025-11-03 21:01:33 -0300

    Update funcoesArnaldo.cpp

--- funcoesArnaldo.cpp
+++ funcoesArnaldo.cpp
@@ -7,19 +7,26 @@ struct carta
 {
 	string nome;
 	int forca, defesa, magia;
-}
-carta[] ler_arquivos()
+};
+carta ler_arquivos(carta[])
 {
-	carta[100] = [];
+	
 	string entrada_string;
-	int entrada_int;
-	ifstream leitura
+	int entrada_forca, entrada_magia, entrada_defesa;
+	int count = 0;
+	ifstream leitura;
 	arq.open("cartas.txt", ifstream::in);
 	while(!arq.eof())
 	{
-	leitura >> 	
+	leitura >> 	entrada_string;
+	leitura >> entrada_forca >> entrada_defesa >> entrada_magia;
+	carta[count].nome = entrada_string;
+	carta[count].forca = entrada_forca;
+	carta[count].defesa = entrada_defesa;
+	carta[count].magia = entrada_magia;
 	}
-	
+	arq.close();
+	return carta;
 }
 
 
@@ -29,7 +36,8 @@ carta[] ler_arquivos()
 int main()
 {
 	
-	
+	carta[100] = [];
+	ler_arquivos(carta);
 	
 	return 0;
 }
