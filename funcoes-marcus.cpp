a78b444abd4ae729d29474eb32b0f26a1f861e05
marcusviniciusrbr-ship-it <marcusviniciusrbr@gmail.com>
2025-11-05 23:16:33 -0300

    Update funcoes-marcus.cpp

--- funcoes-marcus.cpp
+++ funcoes-marcus.cpp
@@ -11,40 +11,24 @@ struct carta
 	int defesa;
 	int magia;
 };
-// Separar em duas funções, e retirar o for e o vetor
+
+void Ler_Carta(string nome, int forca, int defesa, int magia)
+{
+	ofstream arq1;
+	arq1.open("cartas.txt", ofstream :: app);
+	arq1 << nome << endl;
+	arq1 << forca << endl;
+	arq1 << defesa << endl;
+	arq1 << magia << endl;
+}
+
 int main()
 {
-	carta Vet[2];
+	carta carta1;
 	
-	for(int i = 0; i < 2; i++)
-	{
-		cout << "Nome carta " << i + 1 << endl;
-		cin >> Vet[i].nome;
-		
-		cout << "Forca carta " << i + 1 << endl;
-		cin >> Vet[i].forca;
-		
-		cout << "Defesa carta " << i + 1 << endl;
-		cin >> Vet[i].defesa;
-		
-		cout << "Magia carta " << i + 1 << endl;
-		cin >> Vet[i].magia;
-	}
+	cin >> carta1.nome >> carta1.forca >> carta1.defesa >> carta1.magia;
 	
-	ofstream arq1;
-	arq1.open("cartas.txt", ofstream :: out);
+	Ler_Carta(carta1.nome, carta1.forca, carta1.defesa, carta1.magia);
 	
-	for(int i = 0; i < 2; i++)
-	{
-		arq1 << Vet[i].nome << endl;
-		arq1 << Vet[i].forca << endl;
-		arq1 << Vet[i].defesa << endl;
-		// remover o segundo endl
-		arq1 << Vet[i].magia << endl << endl;
-		
-	}
-	
-	
-	return 0;
-	
-	}
+	return 0;	
+}
