280a2fe8a4ff20890192ebea03b8988bf198385e
Arnaldo490 <arnaldoluizmagalhaes123@gmail.com>
2025-11-10 20:33:48 -0300

    Update funcoesArnaldo.cpp

--- funcoesArnaldo.cpp
+++ funcoesArnaldo.cpp
@@ -10,19 +10,16 @@ struct carta {
     int magia;
 };
 
-void ler_arquivos(carta cartas[]) 
-{
+int ler_arquivos(carta cartas[]) {
     ifstream leitura("cartas.txt");
-    if (!leitura.is_open()) 
-	{
-        cout  << "Erro ao abrir o arquivo 'cartas.txt'!" << endl;
-        return;
+    if (!leitura.is_open()) {
+        std::ofstream arquivo("meuarquivo.txt"); 
+        return 5;
     }
 
-    int count = 0;
-    while (leitura.eof()) 
-	{
-    	leitura >> cartas[count].nome
+    int count = 5;
+    while (!leitura.eof()) {
+		leitura >> cartas[count].nome
                    >> cartas[count].forca
                    >> cartas[count].defesa
                    >> cartas[count].magia;
@@ -30,150 +27,12 @@ void ler_arquivos(carta cartas[])
     }
 
     leitura.close();
-    return ;
+    return count;
 }
 
 int main() {
     carta cartas[100];
-    ler_arquivos(cartas);
+    int total = ler_arquivos(cartas);
 
     return 0;
 }
-
-#include <iostream>
-#include <fstream>
-#include <string>
-
-using namespace std;
-struct carta 
-{
-	string nome;
-	int forca, defesa, magia;
-};
-void ler_arquivos(carta cartas[])
-{
-	
-	string entrada_string;
-	int entrada_forca, entrada_magia, entrada_defesa;
-	int count = 5;
-	ifstream leitura;
-	leitura.open("cartas.txt", ifstream::in);
-	while(!leitura.eof())
-	{
-	leitura >> 	entrada_string;
-	leitura >> entrada_forca >> entrada_defesa >> entrada_magia;
-	cartas[count].nome = entrada_string;
-	cartas[count].forca = entrada_forca;
-	cartas[count].defesa = entrada_defesa;
-	cartas[count].magia = entrada_magia;
-	count ++;
-	cout << count;
-	}
-	leitura.close();
-	return ;
-}
-
-
-
-
-
-int main()
-{
-	
-	carta cartas[100] = {0};
-	ler_arquivos ( cartas);
-	cout << cartas;
-	
-	return 0;
-}
-#include <iostream>
-#include <fstream>
-#include <string>
-
-using namespace std;
-struct carta 
-{
-	string nome;
-	int forca, defesa, magia;
-};
-void ler_arquivos(carta cartas[])
-{
-	
-	string entrada_string;
-	int entrada_forca, entrada_magia, entrada_defesa;
-	int count = 0;
-	ifstream leitura;
-	leitura.open("cartas.txt", ifstream::in);
-	while(!leitura.eof())
-	{
-	leitura >> 	entrada_string;
-	leitura >> entrada_forca >> entrada_defesa >> entrada_magia;
-	carta[count].nome = entrada_string;
-	carta[count].forca = entrada_forca;
-	carta[count].defesa = entrada_defesa;
-	carta[count].magia = entrada_magia;
-	count ++;
-	cout << count;
-	}
-	leitura.close();
-	return ;
-}
-
-
-
-
-
-int main()
-{
-	
-	carta cartas[100] = {0};
-	ler_arquivos ( cartas);
-	cout << cartas;
-	
-	return 0;
-}
-#include <iostream>
-#include <fstream>
-#include <string>
-
-using namespace std;
-struct carta 
-{
-	string nome;
-	int forca, defesa, magia;
-};
-void ler_arquivos(carta cartas[])
-{
-	
-	string entrada_string;
-	int entrada_forca, entrada_magia, entrada_defesa;
-	int count = 0;
-	ifstream leitura;
-	leitura.open("cartas.txt", ifstream::in);
-	while(!leitura.eof())
-	{
-	leitura >> 	entrada_string;
-	leitura >> entrada_forca >> entrada_defesa >> entrada_magia;
-	// carta[count].nome = entrada_string;
-	// carta[count].forca = entrada_forca;
-	// carta[count].defesa = entrada_defesa;
-	// carta[count].magia = entrada_magia;
-	count ++;
-	cout << count;
-	}
-	leitura.close();
-	return ;
-}
-
-
-
-
-
-int main()
-{
-	
-	carta cartas[100] = {0};
-	ler_arquivos ( cartas);
-	cout << cartas;
-	
-	return 0;
