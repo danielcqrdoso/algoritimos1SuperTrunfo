db1e89c73b2f4592b7a0dedf97458b5d5bc7fcc2
GusLLima <andradegustavo812@gmail.com>
2025-11-10 19:47:30 -0300

    Update funcoes-gustavo.cpp

--- funcoes-gustavo.cpp
+++ funcoes-gustavo.cpp
@@ -1,42 +1,4 @@
-#include <iostream>
-#include <string>
-#include <fstream>
 
-using namespace std;
-
-struct carta
-{
-    string nome;
-    int forca;
-    int defesa;
-    int magia;
-};
-
-void Ler_Carta(string nome, int forca, int defesa, int magia)
-{
-    ofstream arq1("cartas.txt", ofstream::app);
-    arq1 << nome << endl;
-    arq1 << forca << endl;
-    arq1 << defesa << endl;
-    arq1 << magia << endl;
-    arq1.close();
-}
-
-
-int ler_arquivos(carta cartas[], int maximo)
-{
-    ifstream leitura("cartas.txt");
-    
-
-int count = 0; 
-
-while (!leitura.eof() && count < maximo) 
-
-{ leitura >> cartas[count].nome; leitura >> cartas[count].forca; leitura >> cartas[count].defesa; leitura >> cartas[count].magia;
-
-    leitura.close();
-    return count;
-}
 
 int main()
 {
