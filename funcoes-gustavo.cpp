12919fe31f486880267bbc4e98d2363d8cae776f
GusLLima <andradegustavo812@gmail.com>
2025-11-19 21:34:29 -0300

    Update funcoes-gustavo.cpp

--- funcoes-gustavo.cpp
+++ funcoes-gustavo.cpp
@@ -1,38 +1,52 @@
+
 #include <iostream>
 #include <string>
+#include <vector>
 #include <cstdlib>
-#include <time.h>
-#include <stdio.h>
-#include <stdlib.h> 
+#include <ctime>
 using namespace std;
 
-struct cartas
-{
-	string nome;
-	int forca;
-	int defesa;
-	int magia;
+struct cartas {
+    string nome;
+    int forca;
+    int defesa;
+    int magia;
 };
 
-int selecionar_carta()
+cartas selecionar_carta(vector<int> &numeros_repetidos)
 {
-	int numeros_repetidos = [];
-	cartas cartasIniciais[100] = {
-		{"Player1", 80, 60, 0},
+    srand(time(NULL));
+
+    cartas cartasIniciais[6] = {
+        {"Player1", 80, 60, 0},
         {"Player2", 80, 40, 50},
         {"Player3", 90, 70, 10},
         {"Player4", 90, 50, 0},
         {"Player5", 70, 70, 10},
         {"Player6", 50, 90, 20}
-	};
+    };
 
-	// Salvar qual numero foi escolhido e se na hora de escolher um numero, ele já tiver sido escolhido, gerar outro
-	srand(time(NULL));
-	cout << rand() % 6 - 1 <<endl;
-	
-	return 0;
+    int id;
+    
+    // Sortear sem repetir
+    do {
+        id = rand() % 6;
+    } while (find(numeros_repetidos.begin(), numeros_repetidos.end(), id) != numeros_repetidos.end());
+
+    numeros_repetidos.push_back(id);
+
+    return cartasIniciais[id];
 }
 
-int main(){
-	selecionar_carta();
+int main()
+{
+    vector<int> numeros_repetidos;
+
+    cartas escolhida = selecionar_carta(numeros_repetidos);
+
+    cout << "Carta sorteada:\n";
+    cout << escolhida.nome << " "
+         << escolhida.forca << " "
+         << escolhida.defesa << " "
+         << escolhida.magia << endl;
 }
